79e83cafb2231bea2e8f8619b7aa1463ad0f55b8
LinJinCing <a0910726473@gmail.com>
2015-05-27 22:38:59 +0800

    try

--- src/TabbedSetUp.java
+++ src/TabbedSetUp.java
@@ -6,7 +6,10 @@ public class TabbedSetUp {
 	public static void main(String[] args) {
 		
 		TabbedSpecies tabbedSpecies = new TabbedSpecies();  
-		
+		tabbedSpecies.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		tabbedSpecies.setSize( 900, 800 ); // set frame size
+		tabbedSpecies.setVisible( true ); // display frame
+		System.out.println("WWW");
 	}
 
 }
